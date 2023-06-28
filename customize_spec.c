4718f7275476e1754007e3946e82890b319e2f83
CodeX1991 <oghenewonaholy@gmail.com>
2023-06-28 16:04:10 -0700

    Fix the return value issue

--- customize_spec.c
+++ customize_spec.c
@@ -9,25 +9,26 @@
 
 int print_a_stringS(va_list args)
 {
-	int i = 0;
+	int i = 0, j = 0;
 	char *str = va_arg(args, char *);
 
 	if (str != NULL)
 	{
 		while (str[i])
 		{
-			if (str[i] > 0 && str[i] < 32)
+			if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
 			{
 				_putchar('\\');
 				_putchar('x');
 				_putchar('0');
 				_recursion_int_heX(str[i], 16);
+				j += 3;
 			}
 			else
 				_putchar(str[i]);
 			i++;
 		}
-		return (i);
+		return (i + j);
 	}
 	_putchar('(');
 	_putchar('n');
